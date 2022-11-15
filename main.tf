a91bc469589412a0f0a7a412216242daeabe0858
Waleed-Asad <waleedasad01@gmail.com>
2022-11-15 22:42:03 +0500

    Adding IAM Role for backend configuration

--- main.tf
+++ main.tf
@@ -10,5 +10,6 @@ terraform {
     key = "state/terraform.tfstate"
     dynamodb_table = "terraform-bolt-infra-state"
     region = "us-east-2"
+    role_arn = "arn:aws:iam::294622776641:role/service-role/codebuild-infra-ci-cd--service-role"
   }
 }
